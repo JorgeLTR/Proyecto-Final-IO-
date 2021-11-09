2c9699b4048a6f504eeb61df90c149ec4d244aca
JorgeLTR <jorgeluistr.1234@gmail.com>
2021-11-09 14:38:29 -0600

    update repo

---
+++ Main.java
@@ -0,0 +1,13 @@
+/**
+ * This class it's initial to Simplex
+ */
+public Main{
+
+    /**
+     *@authors: NoemiGP, JorgeLTR
+     *@about: 
+     */
+public static void main(String[] args) {
+    
+    }
+}

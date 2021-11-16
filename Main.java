2edb12e314460d1e5e65d99db90d32209a3253e2
JorgeLTR <jorgeluistr.1234@gmail.com>
2021-11-16 14:29:04 -0600

    update repo

--- Main.java
+++ Main.java
@@ -1,48 +1,82 @@
 /**
  * This class it's initial to Simplex
  */
-public Main{
-
+import java.awt.List;
+import java.util.ArrayList;
+public Proyecto_Simplex{
     /**
-     *@author:- Noemi Guadalupe Hernandez Palacios (NoemiGP) 
-     *@author: Marco Antonio Santiago Perez 
+     *@author: Noemi Guadalupe Hernandez Palacios (NoemiGP) 
+     *@author: Marco Antonio Santiago Perez (KLM0Marck)
      *@author: Jorge Luis Tovar Rebollo (JorgeLTR)
      *@about: This code in java is about the simplex method which 
      is our final project of the Operations Research subject
      */
+public class Problema {
+   TablaSimplex proceso;
+   boolean  accionMax; //maximize = true, minimize = false
+   ArrayList<Restriccion> restricciones = new ArrayList<Restriccion>();
+
+   public Problema(boolean max){
+       accionMax=max;
+   }
+}
+
 public class TablaSimplex {
     //Number of variables 
-    private int NumVariables;
+    private int Num_Var;
     //Number of Restrictions
-    private int NumRestricciones;
+    private int Num_Rest;
     //Table width
-    private int ancho;
+    private int Ancho_tab;
     //Table that will have changes
     private double tabla[][];
     //Table Results
-    protected double resultado[];
-    
+    protected double Resul[];
 
     /*
      * Table Class Builder
      */
     public Tabla(Problema problema) {
         calcularM(problema);
-        NumRestricciones = problema.restricciones.size();
-        casoIgual = new int[NumRestricciones];
-        NumVariables = maximoNumSubindices(problema);
-        ancho = NumVariables +variablesAdicionales(problema) + 1;
-        tabla = new double[NumRestricciones][ancho];
+        Num_Rest = problema.restricciones.size();
+        casoIgual = new int[Num_Rest];
+        Num_Var = maximoNumSubindices(problema);
+        Ancho_tab = Num_Var +variablesAdicionales(problema) + 1;
+        tabla = new double[Num_Rest][Ancho_tab];
         //Table value at startup: "+NumRestricciones +",ancho: "+ancho);
-        int k = NumVariables;
-        for (int i = 0; i < NumRestricciones; i++) {
+        int k = Num_Var;
+        for (int i = 0; i < Num_Rest; i++) {
             //Incorporate subscripts
-            for (int ii = 0; ii < NumVariables; ii++) {
+            for (int ii = 0; ii < Num_Var; ii++) {
                 try {
                     tabla[i][ii] = problema.restricciones.get(i).subindices[ii];
                 } catch (Exception ex) {
                     tabla[i][ii] = 0;
                 }
             }
+            //Incorporate slack variables
+            if (problema.restricciones.get(i).valorZ != 1)/*Except in the objective function*/ {
+                if (variablePorSigno(problema.restricciones.get(i).desigualdad) == 1)/*In case of <= o =*/ {
+                    tabla[i][k] = 1;
+                    if (problema.restricciones.get(i).desigualdad == 0)/*In the same way, M is also added*/ {
+                        tabla[0][k] = M;
+                        casoIgual[i] = 1;
+                    }
+                    k += 1;
+                }
+                if (variablePorSigno(problema.restricciones.get(i).desigualdad) == 2)/*In case of >=*/{
+                    tabla[i][k] = -1;
+                    tabla[i][k + 1] = 1;
+                    k += 2;
+                }
+            }
+            //Incorporate result
+            tabla[i][Ancho_tab - 2] = problema.restricciones.get(i).solucion;
+            //Incorporate Z
+            tabla[i][Ancho_tab - 1] = problema.restricciones.get(i).valorZ;
+        }
+    }
+
     }
 }
+
